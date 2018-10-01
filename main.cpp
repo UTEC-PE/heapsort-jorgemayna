daa243748b84ced727ed765704aba79cc14aee7d
Luis Talavera <luis@yodopay.com>
2018-09-30 22:44:04 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <random>
+#include <assert.h>
+#include <string.h>
+
+using namespace std;
+
+#define MIN 100
+#define MAX 500
+
+mt19937 rng;
+
+int generateRandomInt(int min, int max);
+void printArray(int *array, size_t size);
+void heapsort(int* array, size_t size);
+
+int main(int argc, char *argv[]) {
+    rng.seed(random_device()());
+    cout << "===========================================================" << endl;
+    cout << "\tHeapsort Practice" << endl;
+    cout << "===========================================================" << endl << endl;
+
+    const int numberOfElements = generateRandomInt(MIN, MAX);
+    int *numbers = new int[numberOfElements];
+    for (int i = 0; i < numberOfElements; i++) {
+        numbers[i] = generateRandomInt(0, 100);
+    }
+
+    printArray(numbers, numberOfElements);
+    heapsort(numbers, numberOfElements);
+    printArray(numbers, numberOfElements);
+
+    system("read");
+    return EXIT_SUCCESS;
+}
+
+int generateRandomInt(int min, int max) {
+    uniform_int_distribution<mt19937::result_type> distribution(min, max); 
+    return distribution(rng);
+}
+
+void printArray(int *array, size_t size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+void heapsort(int* array, size_t size) {
+    // TODO
+}
